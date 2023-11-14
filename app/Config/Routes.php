487c14b25706e1023d2401df2bf050a85a45a7f3
unknown <academy4@talentamp.com>
2023-11-14 21:58:38 +0800

    Will continue at work. Not working here at home

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('/', 'Home::index');
+
+//students routes
+
+$routes-> get('/users', 'UsersController::index');
+$routes-> get('/users/create', 'UsersController::createUser');
