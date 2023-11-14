d3cc4192197f89f855d82730937beacbbaa15f20
Ralph2 <academy4@talentamp.com>
2023-11-14 14:45:22 +0800

    Made progress

--- app/Controllers/UsersController.php
+++ app/Controllers/UsersController.php
@@ -8,6 +8,37 @@ class UsersController extends BaseController
 {
     public function index()
     {
-        //
+        return view('users/list');
+    }
+
+    public function createUser()
+    {
+        //for viewing
+        return view('users/add');
+    }
+
+    public function storeUser()
+    {
+        //to store user in database
+        return view('users/add');
+    }
+
+    public function editUser($id)
+    {
+        //to insert user in database
+        return view('students/edit');
+    }
+
+
+    public function updateUser($id)
+    {
+        //to update in user database
+
+    }
+
+    public function deleteUser($id)
+    {
+        //to delete in user database
+
     }
 }
